0c4aed4bb794d57467bc0f4e618f22c097a41c09
yumingcan <yumingcan1994@163.com>
2019-06-24 10:11:01 +0800

    Github_EEG_Analysis_Tools

--- Github_EEG_Analysis_Tools/EEG_batch_plot.m
+++
@@ -1,6 +0,0 @@
-function EEG_batch_plot(EEG_plot_xdata,EEG_plot_ydata,Figname,yname)    
-    EEG_plot=figure();
-    plot(EEG_plot_xdata,EEG_plot_ydata,'-',EEG_plot_xdata,EEG_plot_ydata,'*r');
-    xlabel('Frequency(Hz)');
-    ylabel('Norm.Power(%)');
-    saveas(EEG_plot,Figname);
